a9336062a03d824915f52817119226d5d8ad1c2f
SOOYEONIU <149544640+SOOYEONIU@users.noreply.github.com>
2024-03-17 12:20:25 +0900

    Update 입력값 수정 (#2566 최댓값 구하기.js)

--- Code/#2566 최댓값 구하기.js
+++ Code/#2566 최댓값 구하기.js
@@ -1,29 +1,28 @@
-const sampleArr = [
-    [3, 23, 85, 34, 17, 74, 25, 52, 65],
-    [10, 7, 39, 42, 88, 52, 14, 72, 63],
-    [87, 42, 18, 78, 53, 45, 18, 84, 53],
-    [34, 28, 64, 85, 12, 16, 75, 36, 55],
-    [21, 77, 45, 35, 28, 75, 90, 76, 1],
-    [25, 87, 65, 15, 28, 11, 37, 28, 74],
-    [65, 27, 75, 41, 7, 89, 78, 64, 39],
-    [47, 47, 70, 45, 23, 65, 3, 41, 44],
-    [87, 13, 82, 38, 31, 12, 29, 29, 80]
-];
+// const sampleArr = [
+//     [3, 23, 85, 34, 17, 74, 25, 52, 65],
+//     [10, 7, 39, 42, 88, 52, 14, 72, 63],
+//     [87, 42, 18, 78, 53, 45, 18, 84, 53],
+//     [34, 28, 64, 85, 12, 16, 75, 36, 55],
+//     [21, 77, 45, 35, 28, 75, 90, 76, 1],
+//     [25, 87, 65, 15, 28, 11, 37, 28, 74],
+//     [65, 27, 75, 41, 7, 89, 78, 64, 39],
+//     [47, 47, 70, 45, 23, 65, 3, 41, 44],
+//     [87, 13, 82, 38, 31, 12, 29, 29, 80]
+// ];
+//=> 입력받기를 이런 자료구조로 만들기
+
+const inputArr = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n').map(line => line.split(' ').map(item => Number(item)));
 
 //최댓값 변수, 행 번호, 열 번호
 let maxNum, rowNum, columnNum = 0;  
 
-const resultValue = () => {
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            if (sampleArr[i][j] > maxNum) {
-                maxNum = sampleArr[i][j];
-                rowNum = i + 1;
-                columnNum = j + 1;
-            }
-        }//for2
-    }//for1
-    console.log(maxNum);
-    console.log(rowNum + " " + columnNum);
-}//resultValue();
-resultValue();
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        if (inputArr[i][j] > maxNum) {
+            maxNum = inputArr[i][j];
+            [rowNum, columnNum] = [i + 1, j + 1];
+        }
+    }//for2
+}//for1
+console.log(maxNum);
+console.log(rowNum + " " + columnNum);
