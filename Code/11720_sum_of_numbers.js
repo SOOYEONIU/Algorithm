e518b21683cf49eefb3b67f4f9198f3bea485dab
SOOYEONIU <149544640+SOOYEONIU@users.noreply.github.com>
2024-03-18 23:30:10 +0900

    Update 11720_sum_of_numbers.js
    주석 추가

--- Code/11720_sum_of_numbers.js
+++ Code/11720_sum_of_numbers.js
@@ -1,3 +1,4 @@
+//input
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync( filePath ).toString().trim().split('\n');
@@ -7,4 +8,5 @@ let [sum, inputNum] = [0, parseInt(input[0])];
 for ( let i = 0; i < inputNum; i++) {
     sum += parseInt(input[1][i]);
 }
+//output
 console.log(sum);
