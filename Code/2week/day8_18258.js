f39607cdba2fc73199fe64c9ed6e69b740f872bb
SOOYEONIU <149544640+SOOYEONIU@users.noreply.github.com>
2024-03-25 22:02:26 +0900

    Update day8_18258.js
    클래스를 사용하지 않고 queue 배열을 만들었더니, 시간 초과 결과가 나왔다.

--- Code/2week/day8_18258.js
+++ Code/2week/day8_18258.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
+const queue = [];
+
+let i = 1;
+while (i <= input[0]) { 
+    let inputData = [];
+    inputData = input[i].split(' ');
+
+    switch ( inputData[0] ) { 
+        case 'push':
+            queue.push(inputData[1]);
+            break;
+        case 'pop':
+            queue.length ? console.log(queue.shift()) : console.log(-1);
+            break;
+        case 'size':
+            console.log(queue.length);
+            break;
+        case 'empty':
+            queue.length ? console.log(0) : console.log(1);
+            break;
+        case 'front':
+            queue.length ? console.log(queue[0]) : console.log(-1);
+            break;
+        case 'back':
+            queue.length ? console.log(queue[queue.length - 1]) : console.log(-1);
+            break;
+    }
+    i++;
+}// while 반복문
