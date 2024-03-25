695067b17c05586ce9fd2dc6d85e84067ee33a33
SOOYEONIU <149544640+SOOYEONIU@users.noreply.github.com>
2024-03-25 22:05:41 +0900

    Update day8_18258.js
    class로 queue 설계 후 통과!

--- Code/2week/day8_18258.js
+++ Code/2week/day8_18258.js
@@ -2,32 +2,82 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const queue = [];
+class Node { 
+    constructor(data) { 
+        this.data = data;
+        this.next = null;
+    }//constructor
+}//Node
+
+let lastData = '';
+
+class Queue { 
+    constructor() { 
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }//constructor
+
+    enqueue(data) { 
+        const newNode = new Node(data);
+
+        if (this.first) {
+            this.last.next = newNode;
+            this.last = newNode;
+        } else { 
+            this.first = newNode;
+            this.last = newNode;
+        }
+        this.size++;
+        lastData = data;
+    }//enqueue
+    
+    dequeue() { 
+        const deleteData = this.first;
+        this.first = this.first.next;
+        this.size--;
+
+        return deleteData.data;
+    }//dequeue
+    
+    peek() {
+        return this.first.data;
+    }//peek
+    
+    count() { 
+        return this.size;
+    }//count
+}//Queue
+
+let result = ''; // 출력값 
+const queue = new Queue();
 
 let i = 1;
-while (i <= input[0]) { 
+while (i <= input[0]) {
     let inputData = [];
     inputData = input[i].split(' ');
 
-    switch ( inputData[0] ) { 
+    switch (inputData[0]) {
         case 'push':
-            queue.push(inputData[1]);
+            queue.enqueue(inputData[1]);
             break;
         case 'pop':
-            queue.length ? console.log(queue.shift()) : console.log(-1);
+            result += ( queue.count() > 0 ? queue.dequeue() : -1 ) + "\n" ;
             break;
         case 'size':
-            console.log(queue.length);
+            result += queue.count() + "\n";
             break;
         case 'empty':
-            queue.length ? console.log(0) : console.log(1);
+            result += ( queue.count() == 0 ? 1 : 0) + "\n";
             break;
         case 'front':
-            queue.length ? console.log(queue[0]) : console.log(-1);
+            result += ( queue.count() == 0 ? -1 : queue.peek() ) + "\n";
             break;
         case 'back':
-            queue.length ? console.log(queue[queue.length - 1]) : console.log(-1);
+            result += (queue.count() > 0 ? lastData : -1) + "\n";
             break;
     }
     i++;
 }// while 반복문
+
+console.log(result);
