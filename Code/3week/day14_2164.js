4d1704c74334ba4d90e8f861ec7ff12c6a67f947
SOOYEONIU <149544640+SOOYEONIU@users.noreply.github.com>
2024-04-04 10:18:19 +0900

    [Update] 2164번 2번째 풀이
    array는 시간초과 결과가 나왔기 때문에 linked list 자료구조로 접근하였다. 하지만 결과는 틀렸습니다가 나왔다..
    결과 : 틀렸습니다

--- Code/3week/day14_2164.js
+++ Code/3week/day14_2164.js
@@ -2,20 +2,65 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let N = Number(fs.readFileSync(filePath).toString().trim());
 
-const cardBox = [];
 
-for (let i = 1; i <= N; i++) { 
+class Node { 
+    constructor(data){ 
+        this.data = data;
+        this.next = null;
+    }
+}//Node
+
+class Queue {
+    constructor() { 
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    push(data) { 
+        const newNode = new Node(data);
+        if (this.first != null) {
+            this.last.next = newNode;
+            this.last = newNode;
+        } else { 
+            this.first = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+    }
+
+    shift() { 
+        const deleteData = this.first;
+        if (this.length > 0) {
+            this.first = this.first.next;
+            this.length--;
+
+            return deleteData.data;
+        }
+    }
+
+    peek() { 
+        return this.first.data;
+    }
+
+    size() { 
+        return this.length;
+    }
+}//Queue
+
+const cardBox = new Queue();
+
+for (let i = 1; i <= N; i++){    
     cardBox.push(i);
 }
 
-//나머지 하나 남은 카드 구하는 함수
-const oneCard = (arr) => { 
-    for (let i = 0; i < N; i++) { 
-        arr.shift();
-        arr.push(arr.shift());
-        if (arr.length == 1) { break; }
+let j = 0;
+while (j < N) {
+    cardBox.shift();
+    if (cardBox.size() == 1) {
+        console.log(cardBox.peek());
+        break;
     }
-    if (arr.length == 1) { console.log(arr[0]); }
-}//oneCard
-
-oneCard(cardBox);
+    cardBox.push(cardBox.shift());
+    j++;
+}
