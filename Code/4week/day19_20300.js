8d02b221c0b1dfc30fd08276d60c9c1103385961
kimsooyeon <rlatndus235@gmail.com>
2024-04-12 23:42:06 +0900

    Add : Boj20300 두번째 풀이
    Javascript에서 Number 값이 안정적으로 나타낼 수 있는 값의 최대치인 2**53−1 범위를 넘어가므로 BigInt 내장 객체를 사용하여 sort 정렬 방식을 바꿨다.
    결과 : 맞았습니다! ✅

--- Code/4week/day19_20300.js
+++ Code/4week/day19_20300.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const input = fs.readFileSync(__dirname + "/input.txt").toString().trim().split('\n');
 
 let N = Number(input[0]);
-const arr = input[1].split(' ').map(Number).sort((a,b) => b - a).map((v) => BigInt(v));
+const arr = input[1].split(' ').map(v => BigInt(v));
+arr.sort((a, b) => (a < b ? -1 : 1));
 
 let M = 0n;
 
@@ -10,8 +11,8 @@ if (N % 2 == 1) {
     M = arr.pop();
 }
 
-for (let i = 1; i < (N / 2); i++) { 
-    let sum = arr.pop() + arr.shift(); 
+for (let i = 1; i < (N / 2); i++) {
+    let sum = arr.pop() + arr.shift();
     M = (sum > M) ? sum : M;
 }
 
