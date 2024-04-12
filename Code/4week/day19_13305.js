308c7fac282cb25a38e2b3c35ec4650b97d2fc12
kimsooyeon <rlatndus235@gmail.com>
2024-04-12 16:43:12 +0900

    Test : Boj13305 두번째 풀이
    결과 출력값 맞지만
    결과 : 58점🔥

--- Code/4week/day19_13305.js
+++ Code/4week/day19_13305.js
@@ -3,22 +3,15 @@ let input = fs.readFileSync(__dirname + '/input.txt').toString().trim().split("\
 
 const N = Number(input.shift());
 
-let distanceNum = input.shift().split(' ').map(Number);
-let priceInfo = input[0].split(' ').map(Number);
+let distanceNum = input[0].split(' ').map(Number);
+let priceInfo = input[1].split(' ').map(Number);
 
 let result = 0;
+let currentPrice = priceInfo[0];
 
-for (let i = 0; i < N; i++) { 
-    if (i == N - 1) {
-        break;
-    } else if (i == 0) {
-        result += priceInfo[i] * distanceNum[i];
-    } else if(i > 0) { 
-        if (priceInfo[i - 1] < priceInfo[i]) {
-            result += priceInfo[i - 1] * distanceNum[i];
-        } else {
-            result += priceInfo[i] * distanceNum[i];
-        }
-    }
+for (let i = 0; i < N-1; i++) { 
+    result += currentPrice * distanceNum[i]; 
+
+    currentPrice = currentPrice > priceInfo[i + 1] ? priceInfo[i + 1] : currentPrice;
 } // for
 console.log(result);
