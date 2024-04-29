ddb94464e000e400eef6e72103c80e6dd03d2e82
kimsooyeon <rlatndus235@gmail.com>
2024-04-29 23:58:20 +0900

    Add : boj2606 2번째
    DFS 알고리즘
    결과 : 정답 ✅

--- Code/6week/boj2606.js
+++ Code/6week/boj2606.js
@@ -4,35 +4,32 @@ let input = fs.readFileSync(__dirname + "/input.txt").toString().trim().split("\
 
 const [computerNum, connectLink] = [Number(input.shift()), Number(input.shift())];
 
-let someNum = input.map(num => num.split(' ').map(Number));
+let graph = [...new Array(computerNum + 1)].map(() => []);
+let visited = [...new Array(computerNum + 1)].fill(0);
+console.log(graph, visited)
 
-let numGraph = {};
-
-for (let i = 0; i < someNum.length; i++) { 
-    connectValue = [];
-    if (numGraph[someNum[i][0]] != undefined) {
-        numGraph[someNum[i][0]].push(someNum[i][1]);
-    } else {
-        connectValue.push(someNum[i][1]);
-        numGraph[someNum[i][0]] = connectValue;
-    };
+for (let i = 0; i < connectLink; i++) { 
+    let start = Number(input[i].split(" ")[0]);
+    let end = Number(input[i].split(" ")[1]);
+    console.log(start, end)
+    graph[start].push(end);
+    graph[end].push(start);
 }
+console.log(graph, visited)
 
-resultNum = [];
+visited[1] = 1;
 
-for (key in numGraph) {
-    for (let i = 0; i < numGraph[key].length; i++) { 
-        if (key == 1) {
-            resultNum.push(numGraph[key][i]);
+let ans = 0;
+
+const dfs = (start) => {
+    for (let end of graph[start]) { 
+        if (!visited[end]) { 
+            visited[end] = 1;
+            ans++;
+            dfs(end);
         }
     }
 }
-for (let i = 0; i < numGraph[1].length; i++) {
-    for (let j = 0; j < numGraph[resultNum[i]].length; j++) { 
-        if (!resultNum.includes(numGraph[resultNum[i]][j])) { 
-            resultNum.push(numGraph[resultNum[i]][j])
-        } else { continue; }
-    }
-}
-
-console.log(resultNum.length);
+dfs(1);
+console.log(graph, visited)
+console.log(ans);
