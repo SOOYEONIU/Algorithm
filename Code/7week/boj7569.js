345e51a047650dd508d1bc7ce649f9eb6416bde8
kimsooyeon <rlatndus235@gmail.com>
2024-05-08 11:07:32 +0900

    Add : boj7569 풀이
    결과 : 정답 ✅
    참고하여 풀이함. 다시 스스로 풀어보기

--- Code/7week/boj7569.js
+++ Code/7week/boj7569.js
@@ -5,84 +5,65 @@ let input = fs.readFileSync(__dirname + '/input.txt')
     .split('\n')
     .map(v => v.split(' ').map(Number));
 
-let [m, n, k] = input.shift();
+let [M, N, H] = input.shift();
 
 const chunk = (data, size) => { // 토마토 상자를 3차원 배열로 변환
-    console.log(data.length)
-    if (data.length === size) {
-        return data;
-    } else {
-        const arr = [];
-    
-        for (let i = 0; i < data.length; i += size) {
-            arr.push(data.slice(i, i + size));
-        }
-        return arr;
+    const arr = [];
+    for (let i = 0; i < data.length; i += size) {
+        arr.push(data.slice(i, i + size));
     }
+    return arr;
 }
+// 토마토 상자 3차원 배열 
+let box = chunk(input, N);
+//방문 여부 3차원 배열
+let visited = Array.from(Array(H), () => Array.from(Array(N), ()=> new Array(M).fill(false)));
 
-let box = chunk(input, n); // 토마토 상자
-let visited = Array.from(Array(k), () => Array.from(Array(n), ()=> new Array(m).fill(false)));
-console.log(box ,visited)
-
-let dx = [-1, 0, 1, 0];
-let dy = [0, -1, 0, 1];
-let dz = [-1, 1];
-let result = -1;
-
-const checkedTomato = (x, y, z) => {
-    visited[z][x][y] = true;
-    // 위칸, 아래칸
-    for (let i = 0; i < 2; i++) { 
-        let nz = z + dz[i];
-
-        if (nz >= 0 && nz < k && box[z][x][y] === 1 && box[nz][x][y] === 0) { 
-            box[nz][x][y] = 1;
-            visited[nz][x][y] = true;
-            checkedTomato(x, y, nz);
-        }
-    }
-
-    // 왼,오,앞,뒤
-    for (let i = 0; i < 4; i++) {
-        let nx = x + dx[i];
-        let ny = y + dy[i];
-
-        if (nx >= 0 && nx < n && ny >= 0 && ny < m && box[z][x][y] === 1 && box[z][nx][ny] === 0) {
-            box[z][nx][ny] = 1;
-            checkedTomato(nx, ny, z);
-        }
-    }
-}
-
-console.log(box)
-
-
-const solution = () => {
-    // 저장될 때부터 모든 토마토가 익어있는 상태인지 출력하는 반복문 
-    // 조건은 0이 있느냐 없느냐 있으면 아래의 3중 for문을 돌림, 없으면 result값 0으로 반환
-    for (let i = 0; i < k; i++) {
-        for (let j = 0; j < n; j++) {
-            for (let l = 0; l < m; l++) {
-                if (box[i][j][l] === 0) {
-                    break;
-                } else {
-                    return result = 0;
-                }
+let queue = []; // 익은 토마토의 위치
+let unripe = 0; // 안익은 토마토의 개수
+for (let i = 0; i < H; i++) { 
+    for (let j = 0; j < N; j++) { 
+        for (let k = 0; k < M; k++) { 
+            if (box[i][j][k] === 1) { //box에 있는 익은 토마토(1)일 경우 tomato 배열에 좌표 넣기
+                queue.push([i, j, k, 0]);
             }
-        }
-    }
-
-    if (result === -1) {
-        for (let i = 0; i < k; i++) {
-            for (let j = 0; j < n; j++) {
-                for (let l = 0; l < m; l++) {
-                    if (box[i][j][l] === 1) {    
-                        checkedTomato(j, l, i);
-                    }
-                }
+            if (box[i][j][k] === 0) { //익지 않은 토마토(0)일 경우 unripe 증가 
+                unripe++;
             }
         }
     }
 }
-solution();
+
+if (!unripe) { // 안익은 토마토가 없으면 0을 반환
+    console.log(0);
+} else {
+    let dx = [-1, 1, 0, 0, 0, 0]; //x좌표
+    let dy = [0, 0, -1, 1, 0, 0]; //y좌표
+    let dz = [0, 0, 0, 0, -1, 1]; //z좌표
+
+    //BFS
+    const BFS = (queue) => {
+        let answer = 0; // 걸린 기간(일수) 담을 변수
+        let i = 0; // 큐 인덱스
+        while (queue.length > i) {
+            let [z, x, y, days] = queue[i++];
+
+            //현재 위치 기준 인접한 여섯 곳 탐색 반복문
+            for (let i = 0; i < 6; i++) {
+                const nx = x + dx[i];
+                const ny = y + dy[i];
+                const nz = z + dz[i];
+
+                //해당 위치가 상자(그래프) 범위 밖으로 벗어나지 않았고, 안익은 토마토라면
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && nz >= 0 && nz < H && box[nz][nx][ny] === 0) {
+                    box[nz][nx][ny] = 1; // 토마토 익힘
+                    queue.push([nz, nx, ny, days + 1]); // 큐에 해당 위치와 하루 증가한 값 담기
+                    unripe--; // 안 익은 토마토 개수 감소
+                }
+            }
+            answer = days;
+        }
+        return (unripe ? -1 : answer); // 안 익은 토마토가 있으면 -1 반환, 없으면 걸린 기간(일수) 반환
+    }
+    console.log(BFS(queue));
+}
