52e69819e65d43863f7ac92481c1dc10caa2b9e2
kimsooyeon <rlatndus235@gmail.com>
2024-05-10 16:08:17 +0900

    Add : boj1991
    결과 : 정답 ✅
    DFS의 전위, 중위, 후위 순회

--- Code/7week/boj1991.js
+++ Code/7week/boj1991.js
@@ -6,65 +6,51 @@ let [N, ...input] = fs.readFileSync(__dirname + '/input.txt')
     .map(value => value.split(' '));
 let n = Number(N[0]);
 
-class TreeNode { 
-    constructor(val) { 
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-} // TreeNode
+const tree = {};
+for (let i = 0; i < input.length; i++) { 
+    const [node, left, right] = input[i];
+    tree[node] = [left, right];
+}
 
 //전위 순회(Pre-order)
 let preOrderResult = "";
 const recursivePreOrder = (node) => { 
-    if (node == null) return;
+    if (node == ".") return;
+    const [left, right] = tree[node];
     //루트 노드를 방문
-    preOrderResult += node.val;
+    preOrderResult += node;
     //왼쪽 서브 트리를 후위 순회
-    recursivePreOrder(node.left);
+    recursivePreOrder(left);
     //오른쪽 서브 트리를 후위 순회
-    recursivePreOrder(node.right);
+    recursivePreOrder(right);
 }
 
 //중위 순회(In-order)
 let inOrderResult = "";
 const recursiveInOrder = (node) => { 
-    if (node == null) return;
-
-    recursiveInOrder(node.left);
-    inOrderResult += node.val;
-    recursiveInOrder(node.right);
+    if (node == ".") return;
+    const [left, right] = tree[node];
+    recursiveInOrder(left);
+    inOrderResult += node;
+    recursiveInOrder(right);
 }
 
 //후위 순회(Post-order)
 let postOrderResult = "";
 const recursivePostOrder = (node) => { 
-    if (node == null) return;
-
-    recursivePostOrder(node.left);
-    recursivePostOrder(node.right);
-    postOrderResult += node.val;
+    if (node == ".") return;
+    const [left, right] = tree[node];
+    recursivePostOrder(left);
+    recursivePostOrder(right);
+    postOrderResult += node;
 }
 
 const main = () => { 
-    // const root = new TreeNode('A');
-    // root.left = new TreeNode('B');
-    // root.left.left = new TreeNode('D');
-    // root.right = new TreeNode('C');
-    // root.right.left = new TreeNode('E');
-    // root.right.right = new TreeNode('F');
-    // root.right.right.right = new TreeNode('G');
-    // console.log(root);
-    const root = new TreeNode('A');
-    for (let i = 0; i < input; i++) { 
-
-    }
-
-    recursivePreOrder(root);
+    recursivePreOrder('A');
     console.log(preOrderResult);
-    recursiveInOrder(root);
+    recursiveInOrder('A');
     console.log(inOrderResult);
-    recursivePostOrder(root);
+    recursivePostOrder('A');
     console.log(postOrderResult);
 }
 main();
