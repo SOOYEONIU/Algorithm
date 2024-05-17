9869df4ff43aa49401b231038bd28a009c664a60
kimsooyeon <rlatndus235@gmail.com>
2024-05-17 10:32:46 +0900

    Add : boj2688 반례 발견💡

--- Code/8week/final_exam/boj2688.js
+++ Code/8week/final_exam/boj2688.js
@@ -4,6 +4,9 @@ let [n, ...input] = fs.readFileSync(__dirname + '/input.txt').toString().trim().
 const solution = (n, tableCells) => {
     let collection = [];
     for (let i = 0; i < n; i++) {
+        if (tableCells.sort((a, b) => a - b).map(v => (v === i + 1))) { 
+            collection.push((i + 1));
+        }
         if (tableCells[(tableCells[i] - 1)] === (i + 1)) { 
             collection.push(i + 1);
         }
@@ -12,3 +15,24 @@ const solution = (n, tableCells) => {
     console.log(collection.sort((a, b) => a - b).join('\n'));
 }
 solution(n, input);
+
+`
+반례를 찾음
+입력값
+6
+2
+3
+1
+5
+4
+6
+
+출력값
+6
+1
+2
+3
+4
+5
+6
+`
