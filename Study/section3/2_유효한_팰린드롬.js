0b02043161563324e9d0ad1823ec93801d2752b9
kimsooyeon <rlatndus235@gmail.com>
2024-06-04 15:44:17 +0900

    Add : Infearn 알고리즘 강의 - section3-2 재풀이 ✅

--- Study/section3/2_유효한_팰린드롬.js
+++ Study/section3/2_유효한_팰린드롬.js
@@ -1,27 +1,9 @@
 const solution = (stringDummy) => { 
     let answer = true;
 
-    let dummyArr = [];
-    let tempString = '';
-    for (let i = 0; i < stringDummy.length; i++) { 
-        if (stringDummy[i] === ',' || stringDummy[i] === ';' || stringDummy[i] === ':' ) {
-            dummyArr.push(tempString);
-            tempString = '';
-        } else if (stringDummy[i] === ' ') {
-            continue
-        } else if (i === stringDummy.length - 1) { 
-            tempString += stringDummy[i];
-            dummyArr.push(tempString);
-        }
-        else {
-            tempString += stringDummy[i];
-        }
-    }
-    console.log(dummyArr) // 더미 배열 만들기 완성
-    for (let i = 0; i < dummyArr.length / 2; i++) { 
-        for (let j = 0; j < dummyArr[i].length; j++) { 
-            console.log((dummyArr[i][j].toUpperCase()).charCodeAt() >= 65 && (dummyArr[i][j].toUpperCase()).charCodeAt() <= 90)
-        } 
+    stringDummy = stringDummy.toLowerCase().replace(/[^a-z]/g, '');
+    if (stringDummy.split('').reverse().join('') !== stringDummy) { 
+        answer = false;
     }
 
     return answer === true ? 'YES' : 'NO';
@@ -29,4 +11,4 @@ const solution = (stringDummy) => {
 
 let input = `found7, time: study; Yduts; emit, 7Dnuof`;
 
-solution(input);
+console.log(solution(input));
