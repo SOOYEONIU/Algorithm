a5f9c2e4c37b99c45cf17a286f59433aef9cdd5e
kimsooyeon <rlatndus235@gmail.com>
2024-06-05 15:09:41 +0900

    Add : Infearn 알고리즘 강의 - s4_2. 뒤집은 소수 코드 단축

--- Study/section4/2_뒤집은_소수.js
+++ Study/section4/2_뒤집은_소수.js
@@ -2,16 +2,17 @@ const solution = (tempN, tempArr) => {
     //숫자를 뒤집고 소수를 출력하는 방법
     let answer = [];
     for (let i = 0; i < tempN; i++) {
-        let reNum = '';
+        // let reNum = '';
         let correctNum = true;
-        for (let j = 0; j < tempArr[i].length; j++) { 
-            reNum = tempArr[i][j] + reNum;
-        }
+        // for (let j = 0; j < tempArr[i].length; j++) { 
+            // reNum = tempArr[i][j] + reNum;
+        // }
+        let reNum = tempArr[i].toString().split('').reverse().join('');
 
         if (Number(reNum) === 1) {
             correctNum = false;
         } else {
-            for (let j = 2; j < Number(reNum); j++) {
+            for (let j = 2; j < Number(reNum)/2; j++) {
                 if (Number(reNum % j) === 0) {
                     correctNum = false;
                     break;
