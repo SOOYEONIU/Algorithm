94040fe768ba283878202e1a3bb237f08a9a8682
kimsooyeon <rlatndus235@gmail.com>
2024-06-08 01:31:38 +0900

    Add : section5 - 6.학급 회장 풀이2 ✅
    new Map을 이용한 풀이

--- Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/6_학급_회장(해쉬).js
+++ Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/6_학급_회장(해쉬).js
@@ -1,22 +1,22 @@
 const solution = (tempArr) => { 
     let answer;
     let [studentNum, everyVote]= [tempArr[0], tempArr[1]];
-    let voteCount = {};
-
+    let voteCount = new Map();
+    
     for (let i = 0; i < everyVote.length; i++) { 
-        if (!voteCount[everyVote[i]]) {
-            voteCount[everyVote[i]] = 1;
+        if (!voteCount.has(everyVote[i])) {
+            voteCount.set(everyVote[i], 1);
         } else { 
-            voteCount[everyVote[i]]++;
+            voteCount.set(everyVote[i], voteCount.get(everyVote[i]) + 1);
         }
     }
 
     // 학급 회장 투표 가장 많은 사람 뽑기
     let maxVotes = 0;
-    for (let member in voteCount) { 
-        if (maxVotes < voteCount[member]) { 
+    for (let [member, voteNum] of voteCount) { 
+        if (maxVotes < voteNum) { 
             answer = member;
-            maxVotes = voteCount[member];
+            maxVotes = voteNum;
         }
     }
 
