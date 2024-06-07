57798e64a77fb87665f7a5748c9acbd52a1ddb0e
kimsooyeon <rlatndus235@gmail.com>
2024-06-08 01:11:40 +0900

    Add : section5 - 5.최대매출 풀이2 ✅
    이중 반복문 -> 단일 반복문으로 해결

--- Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5_최대_매출.js
+++ Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5_최대_매출.js
@@ -1,23 +1,23 @@
 const solution = (tempArr) => { 
     let answer = 0;
     let[[N, K], infoArr] = tempArr; // N일 동안 매출 기록, K일 동안 최대 매출액이 얼마인지 구하기
-    let lp= 0;
-    let rp = lp + 2;
+    let left = 0;
+    let right = 0;
+    let maxSum = 0;
 
-    while (rp < N) {
-        let maxSum = 0;
-        for (let i = lp; i <= rp; i++) {
-            maxSum += infoArr[i];
+    while (right < N) {
+        maxSum += infoArr[right++];
+        if ((right - left) === 3) { 
+            answer = answer < maxSum ? maxSum : answer;
+            maxSum -= infoArr[left++]; 
         }
-        lp++;
-        rp = lp + 2;
-
+    
         answer = answer < maxSum ? maxSum : answer;
     }
 
     return answer;
 }
 let input = `10 3
-12 15 11 20 25 10 20 19 13 15`.split('\n').map(ele => ele.split(' ').map(Number));
+12 15 11 20 25 10 50 19 13 15`.split('\n').map(ele => ele.split(' ').map(Number));
 
 console.log(solution(input));
