f1ab2f069649d67ff584c6e9759e9987e718ae3b
kimsooyeon <rlatndus235@gmail.com>
2024-06-09 21:58:44 +0900

    Add : section5 - 7.아나그램 풀이 2
    Inflearn인강 풀이 참고 ✅

--- Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/7_아나그램(해쉬).js
+++ Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/7_아나그램(해쉬).js
@@ -1,25 +1,24 @@
 const solution = (tempInput) => { 
-    let answer = false;
-    let firstMap
-    for (tempString of tempInput) {
-        let newMap = new Map();
+    let answer = "YES";
 
-        for (let i = 0; i < tempString.length; i++) { 
-            if (!newMap.has(tempString[i])) {
-                newMap.set(tempString[i], 1);
-            } else {
-                newMap.set(tempString[i], newMap.get(tempString[i]) + 1);
-            }
-        }
-        let newArray = [...newMap].sort();
-        if (JSON.stringify(newArray) === JSON.stringify(firstMap)) { 
-            answer = true;
+    let [firstSection, secondSection] = [tempInput[0], tempInput[1]];
+    let hashMap = new Map();
+
+    for (let tempStr of firstSection) { 
+        if (!hashMap.has(tempStr)) hashMap.set(tempStr, 1);
+        else hashMap.set(tempStr, hashMap.get(tempStr) + 1);
+    }
+    console.log(hashMap)
+
+    for (let tempStr of secondSection) { 
+        if (!hashMap.has(tempStr) || hashMap.has(tempStr) === 0) {
+            answer = "NO";
             break;
         }
-        firstMap = newArray;
+        hashMap.set(tempStr, hashMap.get(tempStr) - 1);
     }
 
-    return answer ? 'YES' : 'NO';
+    return answer;
 }
 let input = `AbaAeCe
 baeeACA`.split('\n');
