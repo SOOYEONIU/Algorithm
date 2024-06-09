c65bfbced2fc9a07d83a93c80bafd6dbcb9ca8fe
kimsooyeon <rlatndus235@gmail.com>
2024-06-09 23:51:59 +0900

    Add : section5 - 8.모든 아나그램 찾기 풀이 참고 ✅

---
+++ Study/section5(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/8_모든_아나그램_찾기.js
@@ -0,0 +1,41 @@
+const compareMaps = (map1, map2) => { 
+    if (map1.size !== map2.size) return false;
+    for (let [key, val] of map1) { 
+        if (!map2.has(key) || map2.get(key) !== val) return false;
+    }
+    return true;
+}
+const solution = (tempInput) => { 
+    let answer = 0;
+    let [tempStr, subStr] = [tempInput[0], tempInput[1]];
+    let tH = new Map();
+    let sH = new Map();
+    for (let str of subStr) { 
+        if (sH.has(str)) {
+            sH.set(str, sH.get(str) + 1);
+        } else { 
+            sH.set(str, 1);
+        }
+    }
+    let len = subStr.length-1;
+    for (let i = 0; i < len; i++) { 
+        if (tH.has(tempStr[i])) tH.set(tempStr[i], tH.get(tempStr[i]) + 1);
+        else tH.set(tempStr[i], 1);
+    }
+    let lt = 0;
+    for (let rt = len; rt < tempStr.length; rt++) { 
+        if (tH.has(tempStr[rt])) tH.set(tempStr[rt], tH.get(tempStr[rt]) + 1);
+        else tH.set(tempStr[rt], 1);
+
+        if (compareMaps(tH, sH)) answer++;
+        tH.set(tempStr[lt], tH.get(tempStr[lt])-1);
+        if (tH.get(tempStr[lt]) === 0) tH.delete(tempStr[lt]);
+        lt++;
+    }
+
+    return answer;
+}
+let input = `bacaAacba
+abc`.split('\n');
+
+console.log(solution(input));
