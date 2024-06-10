5aad419a285d549803c839ebb00e7809fb691e50
kimsooyeon <rlatndus235@gmail.com>
2024-06-10 11:54:25 +0900

    Add : section7 - 7. 좌표 정렬 풀이 참고 ⭐️
    풀이 : sort 함수 내 조건문으로 코드 길이 단축!

--- Study/section7/7_좌표_정렬.js
+++ Study/section7/7_좌표_정렬.js
@@ -1,11 +1,12 @@
 const solution = (tempCoordinate) => { 
-    tempCoordinate.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < tempCoordinate.length-1; i++) { 
-        if (tempCoordinate[i][0] === tempCoordinate[i + 1][0]
-            && tempCoordinate[i][1] > tempCoordinate[i + 1][1]) { 
-                [tempCoordinate[i], tempCoordinate[i + 1]] = [tempCoordinate[i + 1], tempCoordinate[i]];
+    tempCoordinate.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1];
+        } else {
+            return a[0] - b[0];
         }
-    }
+    });
+    
     tempCoordinate.map(ele => console.log(ele.join(' ')));
 }
 let input = [
