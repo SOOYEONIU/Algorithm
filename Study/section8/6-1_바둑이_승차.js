4b92e54543407b26516f6d15d2852109f078f90e
kimsooyeon <rlatndus235@gmail.com>
2024-06-12 09:06:04 +0900

    Add : section8 - 6. 바둑이 승차 DFS 재풀이 ✅

---
+++ Study/section8/6-1_바둑이_승차.js
@@ -0,0 +1,18 @@
+const solution = (c, weightArr) => { 
+    let n = weightArr.length;
+    let maxNum = Number.MIN_SAFE_INTEGER;
+    const DFS = (level, sum) => { 
+        if (c < sum) return;
+        if (n === level) {
+            maxNum = Math.max(maxNum, sum);
+        } else { 
+            DFS(level + 1, sum + weightArr[level]);
+            DFS(level + 1, sum);
+        }
+    }
+    DFS(0, 0);
+    console.log(maxNum)
+}
+let c = 259;
+let weightArr = [81, 58, 42, 33, 61];
+solution(c, weightArr);
